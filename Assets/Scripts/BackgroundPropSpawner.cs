664e775084e9ef71af04662764d983d0dcad81a3
PC-JUNJIAN\PC_Cl <spyhanfeng@qq.com>
2018-06-08 19:27:33 +0800

    提交

--- Assets/Scripts/BackgroundPropSpawner.cs
+++ Assets/Scripts/BackgroundPropSpawner.cs
@@ -1,24 +1,20 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using System.Collections;
 
-/// <summary>
-/// 控制背景汽车开动
-/// </summary>
-public class BackgroundPropSpawner : MonoBehaviour {
+public class BackgroundPropSpawner : MonoBehaviour
+{
+	public Rigidbody2D backgroundProp;		// The prop to be instantiated.
+	public float leftSpawnPosX;				// The x coordinate of position if it's instantiated on the left.
+	public float rightSpawnPosX;			// The x coordinate of position if it's instantiated on the right.
+	public float minSpawnPosY;              // The lowest possible y coordinate of position.
+    public float maxSpawnPosY;				// The highest possible y coordinate of position.
+	public float minTimeBetweenSpawns;		// The shortest possible time between spawns.
+	public float maxTimeBetweenSpawns;		// The longest possible time between spawns.
+	public float minSpeed;					// The lowest possible speed of the prop.
+	public float maxSpeed;					// The highest possible speeed of the prop.
 
-    [SerializeField] Rigidbody2D backgroundProp;      // The prop to be instantiated.
-    [SerializeField] float leftSpawnPosX;             // The x coordinate of position if it's instantiated on the left.
-    [SerializeField] float rightSpawnPosX;            // The x coordinate of position if it's instantiated on the right.
-    [SerializeField] float minSpawnPosY;              // The lowest possible y coordinate of position.
-    [SerializeField] float maxSpawnPosY;              // The highest possible y coordinate of position.
-    [SerializeField] float minTimeBetweenSpawns;      // The shortest possible time between spawns.
-    [SerializeField] float maxTimeBetweenSpawns;      // The longest possible time between spawns.
-    [SerializeField] float minSpeed;                  // The lowest possible speed of the prop.
-    [SerializeField] float maxSpeed;                  // The highest possible speeed of the prop.
-
-    // Use this for initialization
-    void Start () {
+	void Start ()
+	{
         // Set the random seed so it's not the same each game.
         // 设置随机的种子，所以它不是相同的每一个游戏。
         Random.InitState(System.DateTime.Today.Millisecond);
@@ -26,16 +22,18 @@ public class BackgroundPropSpawner : MonoBehaviour {
         // Start the Spawn coroutine.
         // 开始产生协同程序。
         StartCoroutine("Spawn");
-    }
+	}
 
-    IEnumerator Spawn() {
+
+	IEnumerator Spawn ()
+	{
         // Create a random wait time before the prop is instantiated.
         //创建一个随机的等待时间，然后再实例化。
-        float waitTime = Random.Range(minTimeBetweenSpawns,maxTimeBetweenSpawns);
+        float waitTime = Random.Range(minTimeBetweenSpawns, maxTimeBetweenSpawns);
 
         // Wait for the designated period.
         // 等待指定的时间。
-        yield return new WaitForSeconds(waitTime);
+		yield return new WaitForSeconds(waitTime);
 
         // Randomly decide whether the prop should face left or right.
         // 随机判断道具是否应该向左或向右。
@@ -47,30 +45,30 @@ public class BackgroundPropSpawner : MonoBehaviour {
 
         // Create a random y coordinate for the prop.
         // 为这个道具创建一个随机的y坐标。
-        float posY = Random.Range(minSpawnPosY,maxSpawnPosY);
+        float posY = Random.Range(minSpawnPosY, maxSpawnPosY);
 
         // Set the position the prop should spawn at.
         // 设置该支柱应该衍生的位置。
-        Vector3 spawnPos = new Vector3(posX,posY,transform.position.z);
+        Vector3 spawnPos = new Vector3(posX, posY, transform.position.z);
 
         // Instantiate the prop at the desired position.
         // 实例化支持的位置。
-        Rigidbody2D propInstance = Instantiate(backgroundProp,spawnPos,Quaternion.identity) as Rigidbody2D;
+        Rigidbody2D propInstance = Instantiate(backgroundProp, spawnPos, Quaternion.identity) as Rigidbody2D;
 
         // The sprites for the props all face left.  Therefore, if the prop should be facing right...
         // 所有道具的精灵都在左边。因此，如果支柱应该面向右……
         if (!facingLeft)
-        {
+		{
             // ... flip the scale in the x axis.
             // …在x轴上翻转刻度。
             Vector3 scale = propInstance.transform.localScale;
-            scale.x *= -1;
-            propInstance.transform.localScale = scale;
-        }
+			scale.x *= -1;
+			propInstance.transform.localScale = scale;
+		}
 
         // Create a random speed.
         // 创建一个随机的速度。
-        float speed = Random.Range(minSpeed,maxSpeed);
+        float speed = Random.Range(minSpeed, maxSpeed);
 
         // These speeds would naturally move the prop right, so if it's facing left, multiply the speed by -1.
         // 这些速度会自然地移动道具，所以如果它向左，速度乘以-1。
@@ -78,7 +76,7 @@ public class BackgroundPropSpawner : MonoBehaviour {
 
         // Set the prop's velocity to this speed in the x axis.
         // 将道具的速度设置为x轴的速度。
-        propInstance.velocity = new Vector2(speed,0);
+        propInstance.velocity = new Vector2(speed, 0);
 
         // Restart the coroutine to spawn another prop.
         // 重新启动coroutine以产生另一个道具。
@@ -87,31 +85,30 @@ public class BackgroundPropSpawner : MonoBehaviour {
         // While the prop exists...
         // 而道具的存在……
         while (propInstance != null)
-        {
+		{
             // ... and if it's facing left...
             //…如果它面朝左……
             if (facingLeft)
-            {
+			{
                 // ... and if it's beyond the left spawn position...
                 // …如果它超出了左侧的产卵位置…
                 if (propInstance.transform.position.x < leftSpawnPosX - 0.5f)
                     // ... destroy the prop.
                     // …破坏支撑。
                     Destroy(propInstance.gameObject);
-            }
-            else
-            {
+			}
+			else
+			{
                 // Otherwise, if the prop is facing right and it's beyond the right spawn position...
                 // 否则，如果道具正面朝右，并且它超出了正确的产卵位置…
                 if (propInstance.transform.position.x > rightSpawnPosX + 0.5f)
-                    // ... destroy the prop.
-                    Destroy(propInstance.gameObject);
-            }
+					// ... destroy the prop.
+					Destroy(propInstance.gameObject);
+			}
 
             // Return to this point after the next update.
             // 在下一次更新之后返回到这个点。
             yield return null;
-        }
-    }
-
+		}
+	}
 }
---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,69 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFollow : MonoBehaviour 
+{
+                                    // 在x轴的距离内，玩家可以在摄像机前移动。
+    public float xMargin = 1f;      // 在x轴的距离内，玩家可以在摄像机前移动。
+    public float yMargin = 1f;      // 在y轴上，玩家可以在摄像机前移动。
+    public float xSmooth = 8f;      // 相机在x轴的目标运动中如何平滑地捕捉到它?.
+    public float ySmooth = 8f;      // 相机在y轴上的目标运动是如何顺利地捕捉到的。
+    public Vector2 maxXAndY;        // 相机的最大x和y坐标。
+    public Vector2 minXAndY;        // 相机的最小x和y坐标。
+
+
+    private Transform player;       // Reference to the player's transform.  参考玩家的转换。
+
+
+    void Awake ()
+	{
+		// Setting up the reference.
+		player = GameObject.FindGameObjectWithTag("Player").transform;
+	}
+
+
+	bool CheckXMargin()
+	{
+		// Returns true if the distance between the camera and the player in the x axis is greater than the x margin.
+		return Mathf.Abs(transform.position.x - player.position.x) > xMargin;
+	}
+
+
+	bool CheckYMargin()
+	{
+		// Returns true if the distance between the camera and the player in the y axis is greater than the y margin.
+		return Mathf.Abs(transform.position.y - player.position.y) > yMargin;
+	}
+
+
+	void FixedUpdate ()
+	{
+		TrackPlayer();
+	}
+	
+	
+	void TrackPlayer ()
+	{
+		// By default the target x and y coordinates of the camera are it's current x and y coordinates.
+		float targetX = transform.position.x;
+		float targetY = transform.position.y;
+
+		// If the player has moved beyond the x margin...
+		if(CheckXMargin())
+			// ... the target x coordinate should be a Lerp between the camera's current x position and the player's current x position.
+			targetX = Mathf.Lerp(transform.position.x, player.position.x, xSmooth * Time.deltaTime);
+
+        // If the player has moved beyond the y margin...
+        //如果player已经超出了y的界限…
+        if (CheckYMargin())
+			// ... the target y coordinate should be a Lerp between the camera's current y position and the player's current y position.
+			targetY = Mathf.Lerp(transform.position.y, player.position.y, ySmooth * Time.deltaTime);
+
+        // 目标x和y坐标不应该大于或小于最小值。
+        targetX = Mathf.Clamp(targetX, minXAndY.x, maxXAndY.x);
+		targetY = Mathf.Clamp(targetY, minXAndY.y, maxXAndY.y);
+
+		// Set the camera's position to the target position with the same z component.
+		transform.position = new Vector3(targetX, targetY, transform.position.z);
+	}
+}
